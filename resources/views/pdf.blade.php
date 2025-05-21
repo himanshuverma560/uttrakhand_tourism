9dfabcd7822cfac36d3992a5a2c0857dd2816213
gp0811 <gauravvithu@gmail.com>
2025-05-21 11:16:37 +0530

    pdf ui fixes

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -17,13 +17,12 @@
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 15px;
         }
 
         th,
         td {
             border: 1px solid #000;
-            padding: 6px;
+            padding: 8px 6px;
             vertical-align: top;
         }
 
@@ -36,172 +35,159 @@
 
 <body>
 
-  {{-- Heading Centered on Top --}}
-<div style="text-align: center; margin-bottom: 10px;">
-    <h2 style="margin: 0;">Uttarakhand Tourism Development Board Yatra Registration Letter</h2>
-</div>
+    {{-- Heading Centered on Top --}}
+    <div style="text-align: center; margin-bottom: 10px;">
+        <h2 style="margin: 0;">Uttarakhand Tourism Development Board Yatra Registration Letter</h2>
+    </div>
 
-{{-- Table-based layout for DOMPDF reliability --}}
-<table width="100%">
-    <tr>
-        {{-- Profile Image on Left --}}
-        <td width="25%" align="left">
-            <img src="{{ $data['profile_image_path'] }}" alt="Profile Photo"
-                 style="width: 120px; height: 150px; object-fit: cover;">
-        </td>
+    <div style="border: 1px solid #000;">
+        <div style="width: 100%;height: 170px;">
+            <div style="">
+                <img src="{{ $data['profile_image_path'] }}" alt="Profile Photo"
+                    style="width: 120px; height: 150px; object-fit: cover; float: left;margin-right:250px">
+            </div>
+            <div>
+                <img src="{{ $data['qr_code'] }}" alt="QR Code"
+                    style="width: 150px; height: 150px; object-fit: contain; float: left;">
+            </div>
+        </div>
+        <table>
+            <tr>
+                <td style="width: 50%;">Unique Registration No</td>
+                <td>{{ $data['unique_id'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Group ID</td>
+                <td>{{ $data['tour_id'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Destination</td>
+                <td>{{ $data['destinations'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Tour Days</td>
+                <td>{{ $data['tour_days'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Selected Dham Destination Date</td>
+                <td>{!! nl2br(e($data['date_wise_destination'])) !!}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Full Name</td>
+                <td>{{ $data['name'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Gender</td>
+                <td>{{ $data['gender'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Age</td>
+                <td>{{ $data['age'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Diseases</td>
+                <td>{{ $data['status'] ?? 'NA' }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Aadhar Card Number</td>
+                <td>{{ $data['aadhar_card'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Email Address</td>
+                <td>{{ $data['email'] ?? '-' }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Mobile Number</td>
+                <td>{{ $data['mobile'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Country</td>
+                <td>{{ $data['country'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Address</td>
+                <td>{{ $data['address'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">City</td>
+                <td>{{ $data['city'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">District Name</td>
+                <td>{{ $data['district'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">State</td>
+                <td>{{ $data['state'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Emergency Contact No</td>
+                <td>{{ $data['contact_number'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Contact Person Name</td>
+                <td>{{ $data['contact_person'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Contact Person Relation</td>
+                <td>{{ $data['contact_relation'] }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Profession</td>
+                <td>{{ $data['profession'] ?? '-' }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Mode of Travel for Dham</td>
+                <td>{{ $data['vehicle_details'] ?? '-' }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Driver's Name</td>
+                <td>{{ $data['driver_name'] ?? '-' }}</td>
+            </tr>
+            <tr>
+                <td style="width: 50%;">Vehicle Number</td>
+                <td>{{ $data['vehicle_number'] ?? '-' }}</td>
+            </tr>
+        </table>
+    </div>
+    <h2 style="text-align: left;">Important Directions:-</h2>
 
-        {{-- Empty space in center --}}
-        <td width="25%"></td>
-        
-
-        {{-- QR Code on Right --}}
-        <td width="25%" align="right">
-            <img src="{{ $data['qr_code'] }}" alt="QR Code"
-                 style="width: 150px; height: 150px; object-fit: contain;">
-        </td>
-
-        <td width="25%"></td>
-    </tr>
-</table>
-
-
-    <table>
-        <tr>
-            <td><strong>Unique Registration No</strong></td>
-            <td>{{ $data['unique_id'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Group ID</strong></td>
-            <td>{{ $data['tour_id'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Destination</strong></td>
-            <td>{{ $data['destinations'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Tour Days</strong></td>
-            <td>{{ $data['tour_days'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Selected Dham Destination Date</strong></td>
-            <td>{!! nl2br(e($data['date_wise_destination'])) !!}</td>
-        </tr>
-        <tr>
-            <td><strong>Full Name</strong></td>
-            <td>{{ $data['name'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Gender</strong></td>
-            <td>{{ $data['gender'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Age</strong></td>
-            <td>{{ $data['age'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Diseases</strong></td>
-            <td>{{ $data['status'] ?? 'NA' }}</td>
-        </tr>
-        <tr>
-            <td><strong>Aadhar Card Number</strong></td>
-            <td>{{ $data['aadhar_card'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Email Address</strong></td>
-            <td>{{ $data['email'] ?? '-' }}</td>
-        </tr>
-        <tr>
-            <td><strong>Mobile Number</strong></td>
-            <td>{{ $data['mobile'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Country</strong></td>
-            <td>{{ $data['country'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Address</strong></td>
-            <td>{{ $data['address'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>City</strong></td>
-            <td>{{ $data['city'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>District Name</strong></td>
-            <td>{{ $data['district'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>State</strong></td>
-            <td>{{ $data['state'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Emergency Contact No</strong></td>
-            <td>{{ $data['contact_number'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Contact Person Name</strong></td>
-            <td>{{ $data['contact_person'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Contact Person Relation</strong></td>
-            <td>{{ $data['contact_relation'] }}</td>
-        </tr>
-        <tr>
-            <td><strong>Profession</strong></td>
-            <td>{{ $data['profession'] ?? '-' }}</td>
-        </tr>
-        <tr>
-            <td><strong>Mode of Travel for Dham</strong></td>
-            <td>{{ $data['vehicle_details'] ?? '-' }}</td>
-        </tr>
-        <tr>
-            <td><strong>Driver's Name</strong></td>
-            <td>{{ $data['driver_name'] ?? '-' }}</td>
-        </tr>
-        <tr>
-            <td><strong>Vehicle Number</strong></td>
-            <td>{{ $data['vehicle_number'] ?? '-' }}</td>
-        </tr>
-    </table>
-
-    <h4>Important Directions:-</h4>
-
-    <h5>1. Do and Don'ts -</h5>
+    <h4>1. Do and Don'ts -</h4>
     <table>
         <thead>
             <tr>
-                <th>S.No.</th>
-                <th>Do's</th>
-                <th>Don'ts</th>
+                <th style="width: 10%;">S.No.</th>
+                <th style="width: 55%;">Do's</th>
+                <th style="width: 35%;">Don'ts</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
+            <tr style="text-align: center;">
                 <td>1</td>
                 <td>Compulsory Registration</td>
                 <td>Do not Overspeed in Hills</td>
             </tr>
-            <tr>
+            <tr style="text-align: center;">
                 <td>2</td>
                 <td>Keep the Registration certificate ready at the verification point</td>
                 <td>Do not Litter Garbage</td>
             </tr>
-            <tr>
+            <tr style="text-align: center;">
                 <td>3</td>
                 <td>Collect Dham darshan slot token for smooth and hassle free darshan</td>
                 <td>Do not Consume Alcohol/Tobacco</td>
             </tr>
-            <tr>
+            <tr style="text-align: center;">
                 <td>4</td>
                 <td>Acclimatization strongly recommended for people suffering from chronic disease</td>
                 <td>Do not Drink & Drive</td>
             </tr>
-            <tr>
+            <tr style="text-align: center;">
                 <td>5</td>
                 <td>Carry your prescribed medicines, if Any</td>
                 <td>Do not use Private Vehicles as Taxi</td>
             </tr>
-            <tr>
+            <tr style="text-align: center;">
                 <td>6</td>
                 <td>Park the Vehicle in the Right Place</td>
                 <td>Do not defecate in Open</td>
@@ -209,12 +195,18 @@
         </tbody>
     </table>
 
-    <h5>2. Things to Carry -</h5>
-    <ul>
+    <h4 style="margin-bottom: 5px;">2. Things to Carry -</h4>
+    <ul style="list-style: none;font-size: 16px;margin-bottom:20px">
         <li>a) Warm Cloth (Jacket, Shawl, Gloves, etc)</li>
         <li>b) Valid Personal ID Proof</li>
     </ul>
-
+<hr style="width: 113%;margin-left:-6.5%">
+<div style="width: 100%;display:flex;margin-top:10px;margin-bottom:10px;">
+        <img src="../../public/images/logo_vertical.png" style="float: left;margin-right:170px;" alt="">
+        <h3 style="float: left;margin-right:170px;margin-top:20px">Powered by Ethics Infotech LLP</h3>
+        <img src="../../public/images/pdf_logo.png" style="float: left;" alt="">
+</div>
+<hr style="width: 113%;margin-left:-6.5%;margin-top:80px;">
 </body>
 
 </html>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitf07964c0a4ea2e4d9ca6a48228bed9b1
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf07964c0a4ea2e4d9ca6a48228bed9b1::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
